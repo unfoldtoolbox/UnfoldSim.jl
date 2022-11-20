a04e856cef3b300781b48b98dbac9997ef2e391d
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-11-20 11:53:46 +0100

    fix onset calculation, now overlap is possible

--- src/onset.jl
+++ src/onset.jl
@@ -11,13 +11,10 @@ function gen_onsets(rng,simulation::Simulation, onset::UniformOnset)
     (;n_item, n_subj) = simulation.design
     
 	
-	# min + max fixationlen
-	min_fixationlen = onset.offset + maxlength(simulation.components) - onset.width÷2
-	max_fixationlen = onset.offset + maxlength(simulation.components) + onset.width÷2
-
-	# sample different fixationlens & combine them with epoch lens
-	onsets = rand(deepcopy(rng), min_fixationlen:max_fixationlen, (n_item, n_subj))
+	# sample different onsets
+	onsets = rand(deepcopy(rng), onset.offset:(onset.offset + onset.width), (n_item, n_subj))
 	
+    # accumulate them
 	onsets_accum = accumulate(+, onsets, dims=1)
 	return onsets_accum
 end
