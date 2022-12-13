8fda92b7d3b3947832b7b57ff431e97e54b60604
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2022-12-13 15:32:11 +0000

    missunderstood @kw_args

--- test/setup.jl
+++ test/setup.jl
@@ -8,12 +8,11 @@ function gen_debug_design()
 	# define design parameters
 	n_subj = 20
 	n_item = 100
-	btwn_subj = nothing
+
 	btwn_item = Dict("stimType" => ["A", "B"])
-	both_win = nothing
-	
+		
 	# instantiate the design
-	return MultiSubjectDesign(n_subj, n_item, btwn_subj, btwn_item, both_win)
+	return MultiSubjectDesign(;n_subj=n_subj, n_item=n_item, btwn_item = btwn_item)
 end
 
 function gen_debug_component()
