b89fe54038bfa3b950a2a06401b04c81f154c10e
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-12-13 12:53:56 +0000

    adapt formatting

--- docs/literate/tutorials/simulateERP.jl
+++ docs/literate/tutorials/simulateERP.jl
@@ -8,31 +8,31 @@ using UnfoldMakie
 
 # Let's grab a SingleSubjectDesign and add a continuous predictor
 design = SingleSubjectDesign(;
-        conditions=Dict(:condition=>["car","face"],:continuous=>range(-5,5,length=10))
-        ) |> x->RepeatDesign(x,100);
+    conditions=Dict(:condition=>["car","face"],:continuous=>range(-5,5,length=10))
+    ) |> x->RepeatDesign(x,100);
 
 # Let's make use of the prespecified basis functions, but use different formulas + parameters for each!
 
 # **p100** is unaffected by our design and has amplitude of 5
 p1 =  LinearModelComponent(;
-        basis = p100(),
-        formula = @formula(0~1),
-        β = [5]
-        );
+    basis = p100(),
+    formula = @formula(0~1),
+    β = [5],
+);
 
 # **n170** has a condition effect, faces are more negative than cars
 n1 =  LinearModelComponent(;
-        basis = n170(),
-        formula = @formula(0~1+condition),
-        β = [5,-3]
-        );
+    basis = n170(),
+    formula = @formula(0~1+condition),
+    β = [5,-3],
+);
 # **p300** has a continuous effect, higher continuous values will result in larger P300's
 # include both a linear and a quadratic effect of the continuous variable
 p3 =  LinearModelComponent(;
         basis = p300(),
         formula = @formula(0 ~ 1 + continuous + continuous^2),
-        β = [5,1,0.2]
-        );
+        β = [5,1,0.2],
+);
 
 # Now we can simply combine the components and simulate 
 components = [p1, n1, p3] 
@@ -64,10 +64,10 @@ plot_erp(coeftable(m))
 
 # and now beautifully visualized as marginal betas / predicted ERPs
 f = plot_erp(
-        effects(Dict(:condition => ["car", "face"], :continuous => -5:5), m);
-        mapping = (:color => :continuous, linestyle = :condition, group = :continuous),
-        legend = (; valign = :top, halign = :right, tellwidth = false),
-        categorical_color = false,
+    effects(Dict(:condition => ["car", "face"], :continuous => -5:5), m);
+    mapping = (:color => :continuous, linestyle = :condition, group = :continuous),
+    legend = (; valign = :top, halign = :right, tellwidth = false),
+    categorical_color = false,
 );
 
 # Workaround to separate legend and colorbar (will be fixed in a future UnfoldMakie version)
