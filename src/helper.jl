14e914370fb44b93a95d1c094f970e33afb7e39b
Judith Schepers <judith.schepers@vis.uni-stuttgart.de>
2024-01-10 15:59:21 +0100

    Apply suggestions from code review (JuliaFormatter)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/helper.jl
+++ src/helper.jl
@@ -4,9 +4,9 @@ padarray(arr, len, val)
 """
 padarray(arr::Vector, len::Tuple, val) = padarray(padarray(arr, len[1], val), len[2], val)
 function padarray(arr::Vector, len::Int, val)
-	pad = fill(val, abs(len))
-	arr = len > 0 ? vcat(arr, pad) : vcat(pad, arr)
-	return arr
+    pad = fill(val, abs(len))
+    arr = len > 0 ? vcat(arr, pad) : vcat(pad, arr)
+    return arr
 end
 
 
@@ -47,7 +47,8 @@ end
 	
 Takes an array of 'm' target coordinate vector (size 3) (or vector of vectors) and a matrix (n-by-3) of all available positions, and returns an array of size 'm' containing the indices of the respective items in 'pos' that are nearest to each of the target coordinates.
 """
-closest_src(coords_list::AbstractVector{<:AbstractVector}, pos) = closest_src.(coords_list, Ref(pos))
+closest_src(coords_list::AbstractVector{<:AbstractVector}, pos) =
+    closest_src.(coords_list, Ref(pos))
 
 function closest_src(coords::Vector{<:Real}, pos)
 	s = size(pos)
