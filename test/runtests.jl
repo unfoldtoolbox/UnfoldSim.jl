d5d48f36d51e4613b40da9fad6321eec014ab752
Judith Schepers <judith.schepers@vis.uni-stuttgart.de>
2024-01-10 16:49:34 +0100

    Apply suggestions from code review (JuliaFormatter)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using UnfoldSim
 include("setup.jl")
 
 @testset "UnfoldSim.jl" begin
-	include("component.jl")
-	include("design.jl")
-	include("noise.jl")
-	include("onset.jl")
-	include("simulation.jl")
-	include("helper.jl")
+    include("component.jl")
+    include("design.jl")
+    include("noise.jl")
+    include("onset.jl")
+    include("simulation.jl")
+    include("helper.jl")
 end
