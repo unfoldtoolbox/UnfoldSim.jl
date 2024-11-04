58f8a406361b5a991ae66caf18033ecdcf530237
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2024-11-04 20:00:34 +0100

    fix docs

--- docs/literate/reference/designtypes.jl
+++ docs/literate/reference/designtypes.jl
@@ -40,7 +40,7 @@ design_single_shuffled = SingleSubjectDesign(;
         :stimulus_type => ["natural", "artificial"],
         :contrast_level => range(0, 1, length = 3),
     ),
-    event_order_function = x -> shuffle(MersenneTwister(42), x),
+    event_order_function = shuffle,
 );
 # ```@raw html
 # <details>
