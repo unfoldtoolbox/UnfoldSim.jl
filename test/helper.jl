1483628f2e156d6916e7c4d568f50f76fafc3dbd
maanikmarathe <66105649+maanikmarathe@users.noreply.github.com>
2025-09-02 14:20:25 +0200

    add first tests for eyemovement

--- test/helper.jl
+++ test/helper.jl
@@ -18,4 +18,13 @@
 
 
     end
+
+    @testset "gazevec_from_angle_3d" begin
+        @test UnfoldSim.gazevec_from_angle_3d(0, 0) ≈ [0; 1; 0]
+        @test UnfoldSim.gazevec_from_angle_3d(0,90) ≈ [0; 0; 1]
+        @test UnfoldSim.gazevec_from_angle_3d(90,0) ≈ [1; 0; 0]
+        # @test isapprox(UnfoldSim.gazevec_from_angle_3d(0,0), [0; 1; 0])
+        # @test isapprox(UnfoldSim.gazevec_from_angle_3d(0,90), [0; 0; 1])
+        # @test isapprox(UnfoldSim.gazevec_from_angle_3d(90,0), [1; 0; 0])
+    end
 end
