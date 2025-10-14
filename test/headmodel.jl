fa5f085f509daaad3bb3ede4283735279b414414
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2025-10-14 13:39:14 +0200

    update magnitude to new call

--- test/headmodel.jl
+++ test/headmodel.jl
@@ -7,17 +7,13 @@ end
 @testset "leadfield/magnitude" begin
     lf = leadfield(hart)
     @test isa(lf, AbstractArray)
-    mg = magnitude(hart, type = "norm")
-    mg_lf = magnitude(lf)
-    @test mg == mg_lf
 
+    mg = magnitude(lf)
     or = orientation(hart)
 
     @test isa(lf, AbstractArray)
     mg_man = magnitude(lf, or)
-    mg = magnitude(hart)
     @test mg_man == mg
     @test size(mg) == (231 - 4, 2004)
-
 
 end
